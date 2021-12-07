b6ce4444b5f3ed406339b2fa7a3441c13713a3a2
Vitali <kupratsevich@msa-it.ru>
2021-12-08 00:10:21 +0300

    first commit

---
+++ src/navigation/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from '../screens/Home';
+
+const Stack = createNativeStackNavigator();
+
+export default function AppNavigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{title: 'Пациенты'}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ src/screens/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+export default function Home() {
+  return (
+    <View style={styles.wrapper}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
